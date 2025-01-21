46eafcb635f79e2baeaa8d1ebedd4dc4c4cac6cb
Kyle Beasley <kyle.beasley17@gmail.com>
2025-01-21 16:18:16 -0600

    mobile menu setup

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,4 @@
-<header>
+<header class='header'>
     <p class='logo'>Kyle Beasley</p>
 
     <nav>
@@ -6,4 +6,12 @@
         <a href='./about' class='nav-link'>About</a>
         <a href='./blog' class='nav-link'>Blog</a>
     </nav>
+
+    <div class='hamburger' aria-expanded='false'>
+        <svg style="width:20px;height:20px" viewBox="0 0 20 20">
+            <path fill="#ffffff" d="M1,4 H18 V6 H1 V4 M1,9 H18 V11 H1 V7 M3,14 H18 V16 H1 V14" />
+        </svg>
+    </div>
+
+    <div class='mobile-menu' aria-expanded='false'></div>
 </header>
