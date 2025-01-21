c632346a078f816015f03829436192839b3d9151
Kyle Beasley <kyle.beasley17@gmail.com>
2025-01-21 14:05:21 -0600

    init commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+
+$pageTitle = "Hello World";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title><?php echo $pageTitle ?></title>
+        <link rel="stylesheet" href="./src/styles/main.css">
+    </head>
+    <body>
+        <h1>Kyle Ross Beasley</h1>
+    </body>
+</html>
+
