f775c9f69dff134185fa5be616443291d38592e9
Kyle Beasley <kyle.beasley17@gmail.com>
2025-01-21 15:21:15 -0600

    setup for modular importing

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<header>
+    <p class='logo'>Kyle Beasley</p>
+
+    <nav>
+        <a href='./' class='nav-link'>Home</a>
+        <a href='./about' class='nav-link'>About</a>
+        <a href='./blog' class='nav-link'>Blog</a>
+    </nav>
+</header>
